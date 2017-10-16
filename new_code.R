c9d4f0d1afeb000da5cee29c78e5791e5f3e4efa
Marcela Alfaro-Córdoba <marcela.alfaro@gmail.com>
2017-10-15 19:05:38 -0600

    Add FDA case

--- new_code.R
+++ new_code.R
@@ -6,16 +6,22 @@
 rm(list=ls())
 ##### Load data: #####
 load(file="alldataX.Rdata")
+source(file="functions_tosubmit.R")
 
-## Use only TS in the Atlantic as a response and 9 different data sets. Start with data set 9
-
-data<-(cbind(yy0[[1]], dataX[[1]]))
+## Use only TS in the Atlantic as a response and 9 different data sets. Start with data set 9 (more complex)
+data<-(cbind(yy0[[1]], dataX[[9]]))
 matplot(scale(data),type="l",col=c(1,rep(2,56)))
 matlines(scale(data)[,1])
 
+## OPtion 1: devide the data into training and testing
+
+runGLM(data[,1],data[,-1],1,64,64:65)
+runLASSO(data[,1],data[,-1],1,64,64:65)
+
+
+## Option 2: Moving Windows
+
 #Poisson Regression
-
-
 #LASSO
 #Ridge
 #BMA
